c9ad6324317f62ce45b29984108b96dc98395dff
Võ Hoàng Phúc <phucvo0708@gmail.com>
2022-10-27 17:44:58 +0700

    Added under construction notice for unfinished pages

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
 import Layout from "../components/layout/Layout";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="text-center text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-32 font-semibold">
+      This page is under construction
+    </div>
+  );
 };
 
 AboutPage.getLayout = function getLayout(page) {
--- pages/blueprint.js
+++ pages/blueprint.js
@@ -1,7 +1,11 @@
 import Layout from "../components/layout/Layout";
 
 const BlueprintPage = () => {
-  return <div>BlueprintPage</div>;
+  return (
+    <div className="text-center text-xl md:text-2xl lg:text-3xl xl:text-4xl mt-32 font-semibold">
+      This page is under construction
+    </div>
+  );
 };
 
 BlueprintPage.getLayout = function getLayout(page) {
